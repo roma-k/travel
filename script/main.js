637f2ce7aa9749d9a64875d4e0b53d1463065b2c
roma-k <intere07@yandex.ru>
2023-12-18 19:28:47 +0700

    добавлен скролл на GetExplor

--- script/main.js
+++ script/main.js
@@ -1,6 +1,7 @@
 const loginBtn = document.getElementById('login_btn'),
       popup = document.getElementById('popup'),
-      closeBtn = document.getElementById('close_popup');
+      closeBtn = document.getElementById('close_popup'),
+      getExploreButton = document.getElementById('scrollToBooking');
 
 loginBtn.onclick = () => {
     popup.classList.remove('hidden');
@@ -8,4 +9,13 @@ loginBtn.onclick = () => {
 
 closeBtn.onclick = () => {
     popup.classList.add('hidden');
+}
+
+getExploreButton.onclick = () => {
+    event.preventDefault();
+    window.scrollTo({
+        top: 625,
+        left: 0,
+        behavior: "smooth"
+    })
 }
